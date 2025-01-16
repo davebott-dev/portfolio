df9d0e8c0b1d1832a46e4907dd37621602e37f77
= <bottenbergdave@gmail.com>
2025-01-16 08:42:33 -0500

    styling index- working on third page

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,42 +1,90 @@
-import { useState } from 'react';
-import MegaScroll from 'react-mega-scroll';
+import { useState } from "react";
+import MegaScroll from "react-mega-scroll";
 import GitHubIcon from "@mui/icons-material/Github";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import './App.css'
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import "./App.css";
 
 function Index() {
- 
+  const [background, setBackground] = useState(null);
+
+  const randomColor = () => {
+    const choices = [..."0123456789ABCDEF"];
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += choices[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const codeBlock1 = `const randomColor = () => { 
+    const choices = [..."0123456789ABCDEF"];
+
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += choices[Math.floor(Math.random() * 16)];
+    }
+      return color;`;
+  const codeBlock2 = `<button style={{ background }} onClick={() => setBackground(randomColor)}>
+            Click Me! </button>`;
+
   return (
     <MegaScroll>
-      <section id = "first">
+      <section id="first">
         <div>Hello, I am</div>
         <div>David Bottenberg</div>
         <div>Based in Morrisville, NC</div>
         <div>
-          <GitHubIcon sx={{fontSize:40}}/>
-          <LinkedInIcon sx={{fontSize:40}}/>
-          <InstagramIcon sx={{fontSize:40}}/>
+          <GitHubIcon sx={{ fontSize: 40 }} />
+          <LinkedInIcon sx={{ fontSize: 40 }} />
+          <InstagramIcon sx={{ fontSize: 40 }} />
         </div>
       </section>
-      <section id = "second">
-        <div>About Me</div>
+      <section id="second">
+        <div>
+          <h2>About Me</h2>
+          <div>The Creator</div>
+          <div>The Athlete</div>
+          <div>The Developer</div>
+          <div>The Person</div>
+        </div>
         <div>Tech Skills</div>
         <div>Experiences</div>
       </section>
-      <section id = "third">
+      <section id="third">
         <div>
           <h2>Front End</h2>
-          <p>Proficiently creating responsive and <br/>elegent front end designs</p>
+          <p>
+            Proficiently creating responsive and <br />
+            elegent front end designs
+          </p>
         </div>
         <div>
-          <button>Click Me!</button>
+          <button
+            style={{ background }}
+            onClick={() => setBackground(randomColor)}
+          >
+            Click Me!
+          </button>
+          <div>
+            <code>const [background, setBackground] = useState(null);</code>
+            <code>{codeBlock1}</code>
+            <code>{codeBlock2}</code>
+          </div>
         </div>
         <div>sample</div>
-        <div>Back End</div>
+        <div>
+          <h2>Back End</h2>
+          <p>
+            Specialize in building REST APIs using <br />
+            Prisma ORM (Postgresql), Express, React, and NodeJs
+          </p>
+        </div>
       </section>
     </MegaScroll>
-  )
+  );
 }
 
-export default Index
+export default Index;
