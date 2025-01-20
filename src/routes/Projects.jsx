2d14792a06105329db27a6d9e08ef369c3fb7ec2
= <bottenbergdave@gmail.com>
2025-01-20 16:01:17 -0500

    more updates to project page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,61 +1,159 @@
-
 function Projects() {
-
-
-    return (
-        <div className="project-grid">
-            <h1>Projects</h1>
-            <div className="project-card a">
-                <h2>Project 1 Title</h2>
-                <p>Project 1 Description</p>
-                <div> The image will go here</div>
-                <div>
-                    <button>Code</button><button>Demo</button>
-                </div>
-            </div>
-            <div className="project-card">
-            <h2>Project 2 Title</h2>
-                <p>Project 1 Description</p>
-                <div> The image will go here</div>
-                <div>
-                    <button>Code</button><button>Demo</button>
-                </div>
-            </div>
-            <div className="project-card a">
-            <h2>Project 3 Title</h2>
-                <p>Project 1 Description</p>
-                <div> The image will go here</div>
-                <div>
-                    <button>Code</button><button>Demo</button>
-                </div>
-            </div>
-            <div className="project-card">
-            <h2>Project 4 Title</h2>
-                <p>Project 1 Description</p>
-                <div> The image will go here</div>
-                <div>
-                    <button>Code</button><button>Demo</button>
-                </div>
-            </div>
-            <div className="project-card a">
-            <h2>Project 5 Title</h2>
-                <p>Project 1 Description</p>
-                <div> The image will go here</div>
-                <div>
-                    <button>Code</button><button>Demo</button>
-                </div>
-            </div>
-            <div className="project-card">
-            <h2>Project 6 Title</h2>
-                <p>Project 1 Description</p>
-                <div> The image will go here</div>
-                <div>
-                    <button>Code</button><button>Demo</button>
-                </div>
-            </div>
-
+  return (
+    <div className="project-grid">
+      <h1>Projects</h1>
+      <div className="project-card">
+        <div>
+          <h2>Calculator App</h2>
+          <p>
+            A simple calculator App that is able to perform several simple
+            operations including addition, subtraction, multiplication, and
+            division. I utilized classes in order to create the calculator object
+            and its related operations. {" "}
+          </p>
         </div>
-    )
+        <div> 
+            <img src="./project1.png" alt="" />
+        </div>
+        <div>
+          <button><a href="https://github.com/davebott-dev/calculator/tree/main">Code</a></button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Battleship</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Tic-Tac-Toe</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Pokemon Memory Game</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card a">
+        <div>
+          <h2>CV Generator</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Fake Shopping Application</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Mini-Messageboard</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Inventory Application</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>File Uploader</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Sample Authentication App (using jwt)</h2>
+          <p>Project 1 Description</p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Sample Authenication App (using passport-local)</h2>
+          <p>Sample Authenication App (using passport-local) </p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Full Stack Messaging App</h2>
+          <p>Sample Authenication App (using passport-local) </p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+      <div className="project-card">
+        <div>
+          <h2>Portfolio</h2>
+          <p>Sample Authenication App (using passport-local) </p>
+        </div>
+        <div> The image will go here</div>
+        <div>
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
