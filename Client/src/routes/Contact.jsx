5346d5477f2288f206d8064009a8e4f7f5079364
= <bottenbergdave@gmail.com>
2025-01-23 21:06:13 -0500

    beginning to style the contact page

--- Client/src/routes/Contact.jsx
+++ Client/src/routes/Contact.jsx
@@ -1,26 +1,24 @@
 function Contact() {
   //set up a handle submit form that sends the data to the backend
   return (
-    <div>
+    <div id="contact-form">
+      <div>
       <h1>Contact Me</h1>
-
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input type="text" name="name" id="name" />
-
-        <label htmlFor="email">email: </label>
-        <input type="email" name="email" id="email" />
-
-        <label htmlFor="message">Message: </label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder="thoughts..."
-        ></textarea>
-        <button>Submit</button>
-      </form>
-
-      <a href="/Resume.pdf" download="resume"><button type="button">Resume</button></a>
+        <form>
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="name" id="name" />
+          <label htmlFor="email">email: </label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="message">Message: </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="thoughts..."
+          ></textarea>
+          <button>Submit</button>
+        </form>
+        </div>
+        <a href="/Resume.pdf" download="resume"><button type="button">Resume</button></a>
     </div>
   );
 }
